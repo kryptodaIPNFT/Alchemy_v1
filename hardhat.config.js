34f493116eba0c2c4917c0dfe87858f3313dfe0d
kryptodaIPNFT <91292374+kryptodaIPNFT@users.noreply.github.com>
2022-08-15 16:24:30 +0100

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+require('dotenv').config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/zVr3sBlSBIPJYZlNJ_iqfNBKPruCwZ3W",
+      accounts: ["6060d9904eb4ca4cbbb851904089f40f74b1edde57e6d7725f6c68fc1b96c53c"]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
